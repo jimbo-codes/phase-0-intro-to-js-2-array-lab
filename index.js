5003e417eb5e5e1936395127a0ddc41c1716e159
James Burns <jrburns96@aol.com>
2021-11-02 19:22:32 -0400

    second time aroudn the block

--- index.js
+++ index.js
@@ -1,65 +1,32 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', "Garfield"]
 
-const cats = [
-    "Milo",
-    "Otis",
-    "Garfield",
-]
-function destructivelyAppendCat(name) {
-    cats.push(name);
-    return cats
+function destructivelyAppendCat(name){
+    return cats.push(name);
 }
-function destructivelyPrependCat(name) {
-    cats.unshift(name);
-    return cats
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
 }
-function destructivelyRemoveLastCat() {
-    cats.splice(-1);
-    return cats
+function destructivelyRemoveLastCat(){
+    return cats.pop();
 }
-function destructivelyRemoveFirstCat() {
-    cats.splice(0, 1);
-    return cats
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
 }
-function appendCat(name) {
-    
-    const newcats = [
-        ...cats.slice(),
-        name,
-    ]
-    return newcats
-}
-function prependCat(name) {
-    
-    const newcats1 = [
-        name,
-        ...cats.slice(),
-            ]
-    return newcats1
-}
-function removeLastCat() {
-    
-    const newcats2 = [
-        ...cats.slice(0, cats.length-1),
-            ]
-    return newcats2
-        }
-function removeFirstCat() {
-    const newcats3 = [
-        ...cats.slice(1, cats.length),
-            ]
-    return newcats3
-        }
-        /*
-    describe('removeFirstCat()', function () {
-      it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
-        expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
 
-        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
-  });
-});
-
-
-*/
+function appendCat(name){
+    const cats2 = [...cats, name];
+return cats2
+}
+function prependCat(name){
+    const cats2 = [name, ...cats];
+    return cats2;
+}
+function removeLastCat(){
+    const cats2 = cats.slice(0,cats.length-1);
+    return cats2
+}
+function removeFirstCat(){
+    const cats2= cats.slice(1,cats.length);
+    return cats2;
+}
